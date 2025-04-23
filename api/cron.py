0f3191427bfc379e1fea56bc168e959886dc5f95
RICKY2025BB <309947721@qq.com>
2025-04-23 10:01:51 +0800

    Update cron.py

--- api/cron.py
+++ api/cron.py
@@ -1,6 +1,8 @@
 def handler(request):
     return {
         "statusCode": 200,
-        "headers": { "Content-Type": "application/json" },
-        "body": "✅ Solana RSI 接口运行正常！"
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": '{"message": "✅ Solana RSI 接口运行正常！"}'
     }
